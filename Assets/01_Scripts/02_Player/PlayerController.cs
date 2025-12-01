8b2f2cb5788bfbd4fa4b6439e18c2ff8dcc6c056
yohan <dldygksp9701@gmail.com>
2025-12-01 17:00:16 +0900

    feat 플레이어 이동 화면 이동(미완성)

--- Assets/01_Scripts/02_Player/PlayerController.cs
+++ Assets/01_Scripts/02_Player/PlayerController.cs
@@ -5,13 +5,19 @@ using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
-    [SerializeField] private Vector2 curtransform; //현제 위치
-    private string moveAxisName = "Vertical";
-    private Rigidbody rb;
+    [SerializeField] private Vector2 curtransformInput; //이동스크립트 짤 때 필수
+    [SerializeField] private Transform cameraMoveObject; //카메라 이동을 담당하는 오브젝트
+    private Vector2 mouseDelta;//마우스 이동할 때 필요
+    private Rigidbody rb;//리지드바디
 
     [Header("Option")]
-    [SerializeField] private float speed;
-    [SerializeField] private float jumpSpeed;
+    [SerializeField] private float speed;//이동속도
+    [SerializeField] private float jumpPower;//점프파워
+    [SerializeField] private float mouseSensesivity;//감도
+    [SerializeField] private float MaxRoationX;//X축 최대 각도
+    [SerializeField] private float MinRoationX;//X축 최소 각도
+
+    [SerializeField] private float curX;
     void Start()
     {
         rb = GetComponent<Rigidbody>();
@@ -20,26 +26,50 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
         Move();
+
     }
 
     public void Move()
     {
-        Vector3 dir = transform.forward * curtransform.y + transform.right * curtransform.x;
-        
+        Vector3 dir = transform.forward * curtransformInput.y + transform.right * curtransformInput.x;
+
         dir *= speed;
         dir.y = rb.velocity.y;
         rb.velocity = dir;
     }
 
+    public void Look()
+    {
+        float mouseX = Input.GetAxis("mouse X") * mouseSensesivity;
+        float mouseY = Input.GetAxis("mouse Y");
+
+        transform.Rotate(Vector3.up * mouseX);
+        transform.Rotate(Vector3.right * mouseY);
+        transform.eulerAngles = new Vector3(mouseX, mouseY,0);
+    }
+
     public void InputMove(InputAction.CallbackContext context)
     {
-        if(context.phase == InputActionPhase.Performed)
+        if (context.phase == InputActionPhase.Performed)
         {
-            curtransform = context.ReadValue<Vector2>();
+            curtransformInput = context.ReadValue<Vector2>();
         }
-        if(context.phase == InputActionPhase.Canceled)
+        if (context.phase == InputActionPhase.Canceled)
         {
-            curtransform = Vector2.zero;
+            curtransformInput = Vector2.zero;
         }
+    }
+
+    public void InputJump(InputAction.CallbackContext context)
+    {
+        if (context.phase == InputActionPhase.Started)
+        {
+            rb.velocity = new Vector3(0, jumpPower, 0);
+        }
+    }
+
+    public void InputLook(InputAction.CallbackContext context)
+    {
+        mouseDelta = context.ReadValue<Vector2>();
     }
 }
