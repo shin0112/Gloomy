a8c570231719d900a5f6b19e5ac0f2c3b70f0527
shham03 <65348428+shham03@users.noreply.github.com>
2025-12-01 20:16:10 +0900

    test : 장애물 코드

---
+++ Assets/01_Scripts/03_MiniGame/02_ShadowRun/Obstacles/ObstacleManager.cs
@@ -0,0 +1,13 @@
+using JetBrains.Annotations;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObstacleManager : MonoBehaviour
+{
+    [System.Serializable]
+    public class  ObstacleInfo
+    {
+        
+    }
+}
