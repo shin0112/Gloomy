e24f2a6e5df54a7289e5f45de1a8c47fd0a407cd
SuraJeon <sura6856@naver.com>
2025-12-02 20:03:20 +0900

    feat: 클리어 후 일기작성 기능 추가
    - 클리어테스트 게임오브젝트에서 "2챕 클리어" 버튼 누른 후 게임실행 시 확인 가능

--- Assets/01_Scripts/10_UI/Diary/JournalPageUI.cs
+++ Assets/01_Scripts/10_UI/Diary/JournalPageUI.cs
@@ -1,4 +1,31 @@
-﻿public class JournalPageUI : PageUI
+﻿using System;
+using TMPro;
+using UnityEngine;
+
+public class JournalPageUI : PageUI
 {
     // todo : 일기장 페이지 UI 내용 구성
+    
+    [SerializeField] TMP_InputField journalTextInputField;
+
+    private void Awake()
+    {
+        //
+        //journalTextInputField.onValueChanged
+    }
+
+    protected override void OpenPageInternal()
+    {
+        if (firstOpen)
+        {
+            journalTextInputField.gameObject.SetActive(true);
+            journalTextInputField.ActivateInputField();
+            journalTextInputField.Select();
+        }
+        else
+        {
+            // todo : 추후 기획에 따라 변경 필요
+            journalTextInputField.gameObject.SetActive(false);
+        }
+    }
 }
