3c07452ee0c16e28066cf4872d229ccb32b0bcd1
shin0112 <wnslcosltimo12@gmail.com>
2025-12-02 14:09:37 +0900

    design: 그림자 거리 글씨 포맷 변경

--- Assets/01_Scripts/10_UI/MiniGame/ShadowRun/ShadowRunUI.cs
+++ Assets/01_Scripts/10_UI/MiniGame/ShadowRun/ShadowRunUI.cs
@@ -39,7 +39,7 @@ public class ShadowRunUI : MonoBehaviour
 
     private void OnEnable()
     {
-        _distanceText.text = $"그림자 거리: {0}M";
+        _distanceText.text = $"그림자 거리: {0.00}M";
         _mindPiece.gameObject.SetActive(false);
     }
 
@@ -73,11 +73,11 @@ public class ShadowRunUI : MonoBehaviour
     {
         if (_shadow.HasCaughtTarget)
         {
-            _distanceText.text = $"그림자 거리: {0}M";
+            _distanceText.text = $"그림자 거리: {0.00}M";
             return;
         }
 
-        _distanceText.text = $"그림자 거리: {_shadow.Distance:0}M";
+        _distanceText.text = $"그림자 거리: {_shadow.Distance:0.00}M";
     }
 
     /// <summary>
